86d3468d2323bd826f906d5f61ca890e1f083b7b
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-03-16 12:21:32 +0530

    add: Progrma.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_problems_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to 2nd set of problems !");
+        }
+    }
+}

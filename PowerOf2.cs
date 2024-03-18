e61ec2333f1b2acefe16bd9c95ab3aadf1a6c194
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-03-18 12:22:55 +0530

    updating

--- PowerOf2.cs
+++ PowerOf2.cs
@@ -4,7 +4,7 @@ namespace CSharp_problems_2
 {
     class PowerOf2
     {
-        static void Main(string[] args)
+        public void Powering2()
         {
             Console.WriteLine("Welcome to Power of 2 !");
             Console.Write("Enter 0 number between 0 and 30 = ");
@@ -12,9 +12,24 @@ namespace CSharp_problems_2
 
             if (0 <= exponent && exponent < 31)
             {
-                double res = Math.Pow(2, exponent);
+                for (int i = 0; i <= exponent; i++)
+                    Console.WriteLine($"2 power {i} = {Math.Pow(2, i)}");
+            }
+            else
+            {
+                Console.WriteLine("Enter the number between 0 and 30");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Power of 2 !");
+            Console.Write("Enter 0 number between 0 and 30 = ");
+            int exponent = int.Parse(Console.ReadLine());
 
-                Console.WriteLine($"2 power {exponent} = {res}");
+            if (0 <= exponent && exponent < 31)
+            { 
+                for (int i = 0;i<=exponent;i++)
+                    Console.WriteLine($"2 power {i} = {Math.Pow(2,i)}");
             }
             else
             {

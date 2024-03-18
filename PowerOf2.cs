51d9a96abf0b5c1b6373cd6558ed81ffba0c9e9c
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-03-18 12:14:25 +0530

    add: power of 2

---
+++ PowerOf2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSharp_problems_2
+{
+    class PowerOf2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Power of 2 !");
+            Console.Write("Enter 0 number between 0 and 30 = ");
+            int exponent = int.Parse(Console.ReadLine());
+
+            if (0 <= exponent && exponent < 31)
+            {
+                double res = Math.Pow(2, exponent);
+
+                Console.WriteLine($"2 power {exponent} = {res}");
+            }
+            else
+            {
+                Console.WriteLine("Enter the number between 0 and 30");
+            }
+        }
+    }
+}

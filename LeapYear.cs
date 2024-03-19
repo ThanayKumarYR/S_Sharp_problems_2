b82b84534bacd3eb690b1c0b13bc27ab513c9561
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-03-19 18:08:33 +0530

    update: refactoring code

--- LeapYear.cs
+++ LeapYear.cs
@@ -11,47 +11,47 @@ namespace CSharp_problems_2
         static void Main()
         {
 
+           
+            //try
+            //{ 
+            //    Console.WriteLine("Welcome to leap year program !");
+            //    Console.Write("Enter the year: ");
+            //    string yearStr = Console.ReadLine();
+            //    if (yearStr.Length != 4)
+            //    {
+            //        Console.WriteLine("Please enter a 4 digit year !");
+            //        return;
+            //    }
 
-            try
-            {
-                Console.WriteLine("Welcome to leap year program !");
-                Console.Write("Enter the year: ");
-                string yearStr = Console.ReadLine();
-                if (yearStr.Length != 4)
-                {
-                    Console.WriteLine("Please enter a 4 digit year !");
-                    return;
-                }
+            //    int year = int.Parse(yearStr);
 
-                int year = int.Parse(yearStr);
-
-                if (year % 4 == 0)
-                {
-                    if (year % 100 == 0)
-                    {
-                        if (year % 400 == 0)
-                        {
-                            Console.WriteLine($"{year} is leap year !");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"{year} is not leap year!");
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine($"{year} is leap year !");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"{year} is not leap year!");
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            //    if (year % 4 == 0)
+            //    {
+            //        if (year % 100 == 0)
+            //        {
+            //            if (year % 400 == 0)
+            //            {
+            //                Console.WriteLine($"{year} is leap year !");
+            //            }
+            //            else
+            //            {
+            //                Console.WriteLine($"{year} is not leap year!");
+            //            }
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine($"{year} is leap year !");
+            //        }
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{year} is not leap year!");
+            //    }
+            //}
+            //catch(Exception ex) 
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
 
         }
 

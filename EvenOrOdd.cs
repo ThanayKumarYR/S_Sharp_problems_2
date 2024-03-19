ccc2a75824f17937a5a483b3d8abff2f4cc06813
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-03-19 19:37:50 +0530

    add: checks even or odd

---
+++ EvenOrOdd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_problems_2
+{
+    class EvenOrOdd
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number = ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number%2 == 0)
+            {
+                Console.WriteLine($"{number} is even !");
+            }
+            else 
+            {
+                Console.WriteLine($"{number} is odd !");
+            }
+        }
+    }
+}

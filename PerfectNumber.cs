d0a24f90adb35bcbdb116390652a28f9bf22f35f
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-03-19 21:29:57 +0530

    checks if perfect number or not

---
+++ PerfectNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_problems_2
+{
+    class PerfectNumber
+    {
+        static void Main(string[] args)
+        {
+            int summ = 0;
+            Console.Write("Enter the number = ");
+            int number = int.Parse(Console.ReadLine());
+
+
+            for (int i = 1; i < number; i++)
+            { 
+                if(number % i == 0)  summ += i;
+            }
+
+            if (number == summ) Console.WriteLine($"{number} is perfect number");
+            else Console.WriteLine($"{number} is not perfect number");
+        }
+    }
+}

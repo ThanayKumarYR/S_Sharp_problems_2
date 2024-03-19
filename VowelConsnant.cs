d5ad8a77df5832855ef2ab528f83dc2a20fa1c18
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-03-19 19:57:23 +0530

    add: checks if an alpabet is vowel or consnant

---
+++ VowelConsnant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_problems_2
+{
+    class VowelConsnant
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an Alpabet = ");
+            char alpabet = char.Parse(Console.ReadLine());
+
+            if (('a' <= alpabet && alpabet <= 'z') || ('A' <= alpabet && alpabet <= 'Z'))
+            {
+                if (alpabet == 'a' || alpabet == 'e' || alpabet == 'i' || alpabet == 'o' || alpabet == 'u' || alpabet == 'A' || alpabet == 'E' || alpabet == 'I' || alpabet == 'O' || alpabet == 'U')
+                {
+                    Console.WriteLine($"{alpabet} is an vowel !");
+                }
+                else Console.WriteLine($"{alpabet} is a consnant !");
+            }
+            else Console.WriteLine("Invalid input ! Enter an alpabet.");
+        }
+    }
+}

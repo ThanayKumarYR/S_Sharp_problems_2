cd6caf43f10c656ddca7ee2230634a1e229ea07a
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-03-19 22:01:31 +0530

    add: this can reverse any given number

---
+++ ReverseNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_problems_2
+{
+    class ReverseNumber
+    {
+        static void Main(string[] args)
+        {
+            long rem, sum = 0;
+            
+            Console.Write("Enter the number = ");
+            long number = long.Parse(Console.ReadLine());
+            long copyOfNumber = number;
+            while(number != 0)
+            {
+                rem = number % 10;
+                sum += rem;
+                number /= 10;
+                if (number != 0) sum *= 10;
+            }
+            Console.WriteLine($"reverse of {copyOfNumber} is {sum}");
+        }
+    }
+}

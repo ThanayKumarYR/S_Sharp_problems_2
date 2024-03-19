0aa084bb61ea894d69bc8e651a49c5503c581971
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-03-19 19:03:00 +0530

    add: finds prime factors of the given number

---
+++ PrimeFactorization.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_problems_2
+{
+    class PrimeFactorization
+    {
+        public static bool isPrime(int num)
+        { 
+            for (int i = 2;i <= Math.Sqrt(num);i++)
+            { 
+                if (num % i == 0) return false;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Prime Factorization !");
+
+            Console.Write("Enter the number = ");
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    if (isPrime(i)) Console.Write($"{i} ");
+                }
+            }
+
+            Console.WriteLine("\n Thank you !");
+        }
+    }
+}
